93131b46fdf15ddbc0269685c90ddd70da5ba6d8
solv-91 <86408035+Novembo@users.noreply.github.com>
2025-12-19 10:59:47 +0100

    Add build script for dependency installation and setup

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Installer les dépendances
+pip install -r requirements.txt
+
+# Collecter les fichiers statiques
+python manage.py collectstatic --noinput
+
+# Appliquer les migrations
+python manage.py migrate
