897a0a8843c7e7b1e21cbf6a3624989f103d93b3
Novembo <leonardovodouhe91@gmail.com>
2025-12-19 14:52:52 +0100

    Update wsgi file

--- Todo/wsgi.py
+++ Todo/wsgi.py
@@ -5,7 +5,8 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Todo.settings")
 
 application = get_wsgi_application()
 
-# Pour Vercel uniquement
-if __name__ == "__main__":
-    from django.core.management import execute_from_command_line
-    execute_from_command_line(["manage.py", "runserver", "0.0.0.0:8000"])
+app = application  # Pour Vercel uniquement
+
+# if __name__ == "__main__":
+#     from django.core.management import execute_from_command_line
+#     execute_from_command_line(["manage.py", "runserver", "0.0.0.0:8000"])
