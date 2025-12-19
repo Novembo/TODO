eec0bb17ba4fb411c32d9ec17299c97ae73a12a2
solv-91 <86408035+Novembo@users.noreply.github.com>
2025-12-19 10:47:42 +0100

    Modify wsgi.py for Vercel compatibility
    Added support for running the application with Vercel.

--- Todo/wsgi.py
+++ Todo/wsgi.py
@@ -1,6 +1,11 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Todo.settings")
-application = get_wsgi_application()
+
+app = get_wsgi_application()
+
+# Pour Vercel uniquement
+if __name__ == "__main__":
+    from django.core.management import execute_from_command_line
+    execute_from_command_line(["manage.py", "runserver", "0.0.0.0:8000"])
